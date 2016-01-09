aefcb1c60219c66cf0d3a13d31b8ee08749d8d4c
badrish mishra <badrish.kumar.mishra@gmail.com>
2016-01-09 01:00:00 -0600

    changes

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-/**
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /******************************************
 	VPC configuration
  *****************************************/
@@ -39,6 +23,16 @@ module "subnets" {
   secondary_ranges = var.secondary_ranges
 }
 
+/******************************************
+	Routes
+ *****************************************/
+module "routes" {
+  source            = "./modules/routes"
+  project_id        = var.project_id
+  network_name      = module.vpc.network_name
+  routes            = var.routes
+  module_depends_on = [module.subnets.subnets]
+}
 /******************************************
 	Routes
  *****************************************/
